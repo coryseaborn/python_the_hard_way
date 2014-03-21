f4c2dc0aa4e9f118cb650bb31df4226832c1d821
Cory Seaborn <coryseaborn@gmail.com>
2014-03-21 13:29:21 -0700

    updating to use getpass module

--- paramiko_ssh_test.py
+++ paramiko_ssh_test.py
@@ -1,12 +1,21 @@
 import paramiko
+import getpass
+
+# prompts user for password
+pw = getpass.getpass()
+
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
-client.connect("192.168.1.6", username="itadmin", password="Columbia456")
+client.connect("192.168.1.6", username="itadmin", password='%s' % pw)
+
+# alternative to using getpass, just configure the password as it's set below:
+# client.connect("192.168.1.6", username="itadmin", password="password")
+
 stdn, stdout, stderr = client.exec_command('ifconfig en0')
 for line in stdout:
     print line.strip ('\n')
-stdn, stdout, stderr = client.exec_command('ls -ltr')
+stdn, stdout, stderr = client.exec_command('ls -ltr', 'pwd')
 for line in stdout:
     print line.strip ('\n')
 client.close()
