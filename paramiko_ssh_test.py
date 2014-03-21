01a68d1a8ddde3c477d48ca50461f7e579fc0413
Cory Seaborn <coryseaborn@gmail.com>
2014-03-21 13:23:37 -0700

    adding paramiko ssh test

--- paramiko_ssh_test.py
+++ paramiko_ssh_test.py
@@ -2,8 +2,11 @@ import paramiko
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
-client.connect("192.168.1.6", username="itadmin", password="update")
+client.connect("192.168.1.6", username="itadmin", password="Columbia456")
 stdn, stdout, stderr = client.exec_command('ifconfig en0')
+for line in stdout:
+    print line.strip ('\n')
+stdn, stdout, stderr = client.exec_command('ls -ltr')
 for line in stdout:
     print line.strip ('\n')
 client.close()
