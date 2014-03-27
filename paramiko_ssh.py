2243f9ae5169ef48048319297753f5466a710682
coryseaborn <coryseaborn@gmail.com>
2014-03-26 21:51:03 -0700

    updating to simplify

--- paramiko_ssh.py
+++ paramiko_ssh.py
@@ -13,12 +13,12 @@ pw = getpass.getpass()
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
-client.connect(ip, username=usr, password='%s' % pw)
+client.connect(ip, username=usr, password=pw)
 
 # alternative to using getpass, just configure the password as it's set below:
 # client.connect("192.168.1.6", username="itadmin", password="password")
 
-stdn, stdout, stderr = client.exec_command('touch test.txt \n ls -l | grep test')
+stdn, stdout, stderr = client.exec_command('sudo touch /User/%s/Desktop/virus_inject') % usr
 for line in stdout:
     print line.strip ('\n')
 #stdn, stdout, stderr = client.exec_command('ls -ltr')
