bfc7277bcc8484b0d254c26efc44a0fa11c0d0f1
coryseaborn <coryseaborn@gmail.com>
2014-03-26 21:12:02 -0700

    updating paramiko to make it badass

--- paramiko_ssh.py
+++ paramiko_ssh.py
@@ -1,6 +1,11 @@
 # going off of http://dtucker.co.uk/hack/ssh-for-python-in-search-of-api-perfection.html
+from sys import argv
 import paramiko
 import getpass
+
+script, ip, usr = argv
+
+#print "%s" % ip
 
 # prompts user for password
 pw = getpass.getpass()
@@ -8,15 +13,15 @@ pw = getpass.getpass()
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
-client.connect("192.168.1.6", username="itadmin", password='%s' % pw)
+client.connect(ip, username=usr, password='%s' % pw)
 
 # alternative to using getpass, just configure the password as it's set below:
 # client.connect("192.168.1.6", username="itadmin", password="password")
 
-stdn, stdout, stderr = client.exec_command('ifconfig en0')
+stdn, stdout, stderr = client.exec_command('touch test.txt \n ls -l | grep test')
 for line in stdout:
     print line.strip ('\n')
-stdn, stdout, stderr = client.exec_command('ls -ltr')
-for line in stdout:
-    print line.strip ('\n')
+#stdn, stdout, stderr = client.exec_command('ls -ltr')
+#for line in stdout:
+#    print line.strip ('\n')
 client.close()
