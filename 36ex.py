91444552e33bbd3a9a17db2e9abd52c930b3d859
Cory <coryseaborn@gmail.com>
2014-04-05 17:55:54 -0700

    done with 35 doing 36

---
+++ 36ex.py
@@ -0,0 +1,11 @@
+def start():
+    print """*********************************************
+Welcome to the William adventures! Where Will
+explores the neighborhood while TT tries her
+absolute hardest to keep up.
+*********************************************"""
+    print "Will's already in an awesome mood."
+    print "Where would you like to go?"
+    print "1. "
+    
+start()
