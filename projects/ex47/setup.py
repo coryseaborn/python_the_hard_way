e1ac9eaf508ddbbacb37675921011cdc011c16bf
coryseaborn <coryseaborn@gmail.com>
2014-04-11 14:29:34 -0700

    done with 47

---
+++ projects/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distribute.core import setup
+    
+config = {
+    'description': 'My Project',
+    'author': 'My Name',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'] # be sure to change this!
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
